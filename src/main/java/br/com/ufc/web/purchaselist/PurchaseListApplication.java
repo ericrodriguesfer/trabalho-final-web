28ed2d02bf53f3a1ef377fabc2176f2dca0188b4
ericrodriguesfer <er601432@gmail.com>
2021-07-16 17:02:44 -0300

    initied the project and construct base structure

---
+++ src/main/java/br/com/ufc/web/purchaselist/PurchaseListApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufc.web.purchaselist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ufc/web/purchaselist/PurchaseListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufc.web.purchaselist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
