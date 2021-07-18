f0c9ba2f98972c7312581eea02fcb7de36c21106
ericrodriguesfer <er601432@gmail.com>
2021-07-18 15:17:46 -0300

    funtions of user completed

--- src/main/java/br/com/ufc/web/purchaselist/repository/UserRepository.java
+++ src/main/java/br/com/ufc/web/purchaselist/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package br.com.ufc.web.purchaselist.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.ufc.web.purchaselist.entity.UserModel;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
-
+	
+	Optional<UserModel>  findByEmail(String email);
+	boolean existsByEmail(String email);
+	
 }
