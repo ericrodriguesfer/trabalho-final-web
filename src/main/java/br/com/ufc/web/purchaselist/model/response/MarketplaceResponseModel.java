023925ea1ad81453cfc0154dc3de3d546af79bd1
ericrodriguesfer <er601432@gmail.com>
2021-07-19 21:38:52 -0300

    fix uncouples realations entitys database with user

--- src/main/java/br/com/ufc/web/purchaselist/model/response/MarketplaceResponseModel.java
+++ src/main/java/br/com/ufc/web/purchaselist/model/response/MarketplaceResponseModel.java
@@ -5,20 +5,20 @@ public class MarketplaceResponseModel {
 	private long id;
 	private String name;
 	private long quantityProducts;
-	private UserSimplifiedResponseModel user;
+//	private UserSimplifiedResponseModel user;
 	private AdressResponseModel adress;
 	
 	public MarketplaceResponseModel() {
 		super();
 	}
 
-	public MarketplaceResponseModel(long id, String name, long quantityProducts, UserSimplifiedResponseModel user,
+	public MarketplaceResponseModel(long id, String name, long quantityProducts,
 			AdressResponseModel adress) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.quantityProducts = quantityProducts;
-		this.user = user;
+//		this.user = user;
 		this.adress = adress;
 	}
 
@@ -34,9 +34,9 @@ public class MarketplaceResponseModel {
 		return quantityProducts;
 	}
 
-	public UserSimplifiedResponseModel getUser() {
-		return user;
-	}
+//	public UserSimplifiedResponseModel getUser() {
+//		return user;
+//	}
 
 	public AdressResponseModel getAdress() {
 		return adress;
@@ -45,7 +45,7 @@ public class MarketplaceResponseModel {
 	@Override
 	public String toString() {
 		return "MarketplaceResponseModel [id=" + id + ", name=" + name + ", quantityProducts=" + quantityProducts
-				+ ", user=" + user + ", adress=" + adress + "]";
+				+ ", adress=" + adress + "]";
 	}
 	
 }
